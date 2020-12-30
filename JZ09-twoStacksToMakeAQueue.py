bc960d8dbaba631b952fb4eff36ce27eb035b1a5
GeekDream-x <inace0302@gmail.com>
2020-12-31 07:10:23 +0800

    UpdateJZ09

--- JZ09-twoStacksToMakeAQueue.py
+++ JZ09-twoStacksToMakeAQueue.py
@@ -1,3 +1,31 @@
+'''
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+
+ 
+
+示例 1：
+
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+示例 2：
+
+输入：
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+输出：[null,-1,null,null,5,2]
+提示：
+
+1 <= values <= 10000
+最多会对 appendTail、deleteHead 进行 10000 次调用
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
 
 # Your CQueue object will be instantiated and called as such:
 # obj = CQueue()
@@ -26,14 +54,23 @@ class CQueue:
         return temp
 
 
+# 2 376/18.7 99/5  这才是真正用两个stack实现一个队列
+class CQueue:
 
-# 2
-# class CQueue:
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
 
-#     def __init__(self):
+    def appendTail(self, value: int) -> None:
+        self.stack_in.append(value)
 
+    # 只有当out stack空了，才从in stack往里倒入数据
+    def deleteHead(self) -> int:
+        if not self.stack_out:
+            if not self.stack_in:  # 都为空
+                return -1
+            else:  # 把in栈中的东西全部倒入out栈中
+                while self.stack_in:
+                    self.stack_out.append(self.stack_in.pop())
 
-#     def appendTail(self, value: int) -> None:
-
-
-#     def deleteHead(self) -> int:
+        return self.stack_out.pop()

db1b9105eb8c7417a3b1e891671ae545cfaec017
GeekDream-x <inace0302@gmail.com>
2021-04-26 15:46:47 +0800

    UpdateJZ25

--- JZ/JZ25-MergeSortedLinkedList.py
+++ JZ/JZ25-MergeSortedLinkedList.py
@@ -68,6 +68,7 @@ class Solution:
 # 链接：https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/solution/mian-shi-ti-25-he-bing-liang-ge-pai-xu-de-lian-b-2/
 # 来源：力扣（LeetCode）
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         cur = head = ListNode(0)
